e4bc563527c2176c7db703e382d566a3be66bbc5
Gian Borcillo <borcillofg2020@gmail.com>
2022-06-26 22:20:55 +0800

    feat: Make image change when touched from device

--- main.js
+++ main.js
@@ -21,6 +21,21 @@ function changeQuote() {
 	startInterval();
 }
 
+function changeImage() {
+	if (document.readyState !== "complete") return;
+	var element = $(".image");
+
+	if (element.attr("src") == "./assets/image_irl.jpg") {
+		element.stop(true).fadeTo("fast", 0.5, function () {
+			$(this).attr("src", "./assets/image.png");
+		}).fadeTo("fast", 1);
+	} else if (element.attr("src") == "./assets/image.png") {
+		element.stop(true).fadeTo("fast", 0.5, function () {
+			$(this).attr("src", "./assets/image_irl.jpg");
+		}).fadeTo("fast", 1);
+	}
+}
+
 function startInterval() {
 	if (intervalId) return;
 	intervalId = setInterval(changeQuote, 5000);
@@ -30,6 +45,12 @@ function stopInterval() {
 	if (!intervalId) return;
 	clearInterval(intervalId);
 	intervalId = null;
+}
+
+function isTouchable() {
+	return (('ontouchstart' in window) ||
+		(navigator.maxTouchPoints > 0) ||
+		(navigator.msMaxTouchPoints > 0));
 }
 
 window.onload = function () {
@@ -43,18 +64,20 @@ window.onload = function () {
 
 	// Register hover event for some elements
 	$(".image").hover(function () {
-		$(this).stop(true).fadeTo("fast", 0.5, function () {
-			$(this).attr("src", "./assets/image_irl.jpg");
-		}).fadeTo("fast", 1);
-	}, function () {
-		$(this).stop(true).fadeTo("fast", 0.5, function () {
-			$(this).attr("src", "./assets/image.png");
-		}).fadeTo("fast", 1);
+		// Do nothing if device is touchable
+		if (isTouchable()) return;
+		changeImage();
 	});
 	$(".quote").hover(function () {
 		stopInterval();
 	}, function () {
 		startInterval();
+	});
+
+	// Register click event for image if device is touchable
+	$(".image").click(function () {
+		if (!isTouchable()) return;
+		changeImage();
 	});
 
 	// Create tooltips using tippy.js by the data-tippy-content attribute
