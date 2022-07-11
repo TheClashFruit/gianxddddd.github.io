ae65068baa23a0b1a42537689e04176274e5b971
Gian Borcillo <borcillofg2020@gmail.com>
2022-07-11 21:13:42 +0800

    feat: More changes to stuff

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const quotes = ["Software Developer", "Absolute Gamer", "who makes stupid code",
-	"\"Programming is Art.\"", "@gianxddddd", "or MoreGianXD",
-	"\"Deez Nuts got em'\"", "aka Gian Borcillo", "hi you're stupid.",
-	"\"You're visiting a trash website!\"", "\"turi ip ip\"", "\"C/C++ gives me headaches.\"", 
-	"\"I like to drink coca-cola.\"", "Don't doxx me please", "I live inside your walls.",
-	"gift me the valve complete pack", "\"Web Development sucks my friend.\"", "stfu im listening to c418's aria math",
-	"\"Hover or tap the picture.\"", "don't even ask why my monitor is dirty", "\"Very profeshnal idot\""];
+const quotes = ["software developer", "what do you want", "who makes stupid code",
+	"i am a member of ThatCakeID", "@gianxddddd", "or MoreGianXD",
+	"i do powerpoint presentation aswell", "", "okay, what now?",
+	"my friends like to make fun of my last name", "real gaming moment", "is coding a hobby or a job?", 
+	"give me bobux for more info", "don't doxx me please", "i live inside your walls",
+	"gift me the valve complete pack", "webdev sucks my friend", "stfu im listening to c418's aria math",
+	"check out the background lol", "very profeshnal idot"];
 var intervalId;
 
 function changeQuote() {
@@ -16,26 +16,11 @@ function changeQuote() {
 			$(this).text(randomQuote).fadeIn();
 		} else {
 			// If the random quote is equivalent to the current quote, then reset to the default quote
-			$(this).text("\"Professional idiot\"").fadeIn();
+			$(this).text("Professional idiot").fadeIn();
 		}
 	});
 
 	startInterval();
-}
-
-function changeImage() {
-	if (document.readyState !== "complete") return;
-	var element = $(".image");
-
-	if (element.attr("src") == "./assets/image_irl.jpg") {
-		element.stop(true).fadeTo("fast", 0.5, function () {
-			$(this).attr("src", "./assets/image.png");
-		}).fadeTo("fast", 1);
-	} else if (element.attr("src") == "./assets/image.png") {
-		element.stop(true).fadeTo("fast", 0.5, function () {
-			$(this).attr("src", "./assets/image_irl.jpg");
-		}).fadeTo("fast", 1);
-	}
 }
 
 function startInterval() {
@@ -56,25 +41,6 @@ window.onload = function () {
 	});
 	document.addEventListener("blur", function () {
 		stopInterval();
-	});
-
-	// Register hover events for some elements
-	$(".image").hover(function () {
-		changeImage();
-	});
-	$(".quote").hover(function () {
-		stopInterval();
-	}, function () {
-		startInterval();
-	});
-
-	// Register touch event for image if device is touchable
-	// We will use built-in DOM functions here
-	Array.prototype.forEach.call(document.getElementsByClassName("image"), function (element) {
-		element.addEventListener("touchend", function (event) {
-			event.preventDefault();
-			changeImage();
-		});
 	});
 
 	// Create tooltips using tippy.js by the data-tippy-content attribute
